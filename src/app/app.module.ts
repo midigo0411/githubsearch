db4382fef1536ffb4b8d7364490b4ee09f00866c
midigo0411 <midigo041@gmail.com>
2021-05-25 00:02:01 +0300

    add git form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GitFormComponent } from './git-form/git-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GitFormComponent
   ],
   imports: [
     BrowserModule,
