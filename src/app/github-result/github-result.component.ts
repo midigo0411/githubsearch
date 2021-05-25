8c831b9c80d2c3f915fac5489e469182b9e52c12
midigo0411 <midigo041@gmail.com>
2021-05-25 21:32:00 +0300

    add service

---
+++ src/app/github-result/github-result.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-github-result',
+  templateUrl: './github-result.component.html',
+  styleUrls: ['./github-result.component.css']
+})
+export class GithubResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
