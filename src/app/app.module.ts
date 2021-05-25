f8fca9ff7302d27e58f714947078f0483457ea6b
midigo0411 <midigo041@gmail.com>
2021-05-26 01:33:51 +0300

    showing results

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+//import { HttpClient } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,6 +20,7 @@ import { GithubResultComponent } from './github-result/github-result.component';
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
+   // HttpClient
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/github-result/github-result.component.ts
+++ src/app/github-result/github-result.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GithubServiceService } from '../github-search.service'
+import { User } from '../user'
 @Component({
   selector: 'app-github-result',
   templateUrl: './github-result.component.html',
