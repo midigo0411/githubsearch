25aae4c50bca1ae7bebf6d7d4cb9ff8693c1e6b1
midigo0411 <midigo041@gmail.com>
2021-05-25 23:09:24 +0300

    get data from class

--- src/app/github-search.service.ts
+++ src/app/github-search.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { User } from './user';
 import { Repository } from './repository';
+import { getMissingNgModuleMetadataErrorData } from '@angular/compiler';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,7 +12,18 @@ export class GithubServiceService {
   repoData = [];
 
   constructor(private http: HttpClientModule) { 
-    this.user = new User("", 0, "", "", new Date)
+    this.user = new User("", 0, "", "", new Date());
     this.repository= new Repository("", "", new Date(), "", "");
+    
+  }
+  getUserData(username: string){
+    interface ApiResponse{
+      bio:string,
+      public_repos: number, 
+      login: string, 
+      avatar_url: string,
+      created_at: Date
+    
+    }
   }
 }
