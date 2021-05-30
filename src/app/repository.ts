0002290dcba66f8cfe45fe039abd5e94f4d7fe6c
midigo0411 <midigo041@gmail.com>
2021-05-30 21:34:07 +0300

    Auto-generated commit

--- src/app/repository.ts
+++
@@ -1,8 +0,0 @@
-export class Repository {
-    constructor(
-        public name: string,
-        public description: string,
-        public updated_at: Date,
-        public clone_url: string,
-        public language: string){}
-}
